efde10717d71cb5087da8bf98024081ecb03e0f0
ssloth1 <jamesbebarski@gmail.com>
2024-07-21 17:50:00 -0400

    A3 Modifications (Assignment Editor)
    Since we integrated a sample database, instead of having just a filler "everyone" option, I made the component dynamically render the students in the JSON we created. It provides everyone as an option by default (greying out the individual students), and if everyone is unchecked, all students can individually be selected. Individual student selections will be displayed in the component. It is all visual, and nothing is saved to the db.

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,15 +1,12 @@
-import { useParams, Link } from 'react-router-dom';
-import './Editor.css';
-import * as db from '../../Database';
+import { useParams, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./Editor.css";
+import * as db from "../../Database";
 
 type Assignment = {
   _id: string;
   title: string;
   course: string;
-
-  // NTS: the below fields are optional,
-  // just because I'm not really using them yet,
-  // and I don't want big bad compiler to complain
   description?: string;
   points?: number;
   dueDate?: string;
@@ -17,10 +14,52 @@ type Assignment = {
   availableUntil?: string;
 };
 
+// Note to self/note to instructor:
+// I made it so that instead of just having the default "everyone" option, you can also select individual students to assign the assignment to.
+// However, it is locked into everyone by default, to select individual students, you must uncheck the "Everyone" option.
+// Currently, due dates and available to and from dates don't render because the sample assignment data does not have those fields.
+// I imagine that we will have to add those fields down the road when this component is integrated with mongoDB later in the class.
+
 export default function AssignmentEditor() {
+
+  // Get the course and assignment id from the URL, and find the assignment from the database 
   const { cid, aid } = useParams();
   const assignment: Assignment | undefined = db.assignments.find((assignment) => assignment._id === aid);
+  const students = db.users.filter(user => user.role === "STUDENT");
 
+  // State to keep track of who the assignment is assigned, and whether it is assigned to everyone
+  const [assignTo, setAssignTo] = useState<string[]>([]);
+  const [assignEveryone, setAssignEveryone] = useState<boolean>(true);
+
+  // If the assignEveryone state is changed, clear the assignTo state, so that it is not assigned to anyone
+  useEffect(() => {
+    if (assignEveryone) {
+      setAssignTo([]);
+    }
+  }, [assignEveryone]);
+
+  // Function to handle when a student is selected or deselected
+  const renderAssignChange = (id: string) => {
+    if (assignTo.includes(id)) {
+      setAssignTo(assignTo.filter(studentId => studentId !== id));
+    } else {
+      setAssignTo([...assignTo, id]);
+    }
+  };
+
+  // Function to handle when the "Everyone" option is selected or deselected
+  const handleEveryoneChange = () => {
+    setAssignEveryone(!assignEveryone);
+  };
+
+
+  // Get the names of the students that the assignment is assigned to
+  const selectedStudents = assignTo.map(id => {
+    const student = students.find(student => student._id === id);
+    return student ? `${student.firstName} ${student.lastName}` : "";
+  });
+
+  // If the assignment is not found, display a message saying so
   if (!assignment) {
     return (
       <div className="p-3">
@@ -37,7 +76,7 @@ export default function AssignmentEditor() {
       <h5>Assignment Name</h5>
       <input id="wd-name" defaultValue={assignment.title} className="form-control mb-3" />
 
-      <textarea id="wd-description" className="form-control mb-3" defaultValue={assignment.description || ''} />
+      <textarea id="wd-description" className="form-control mb-3" defaultValue={assignment.description || ""} />
 
       <div className="row mb-3 align-items-center">
         <div className="col-md-2">
@@ -59,7 +98,47 @@ export default function AssignmentEditor() {
                 <label htmlFor="wd-assign-to" className="form-label"><strong>Assign to</strong></label>
               </div>
               <div className="col-md-10">
-                <input id="wd-assign-to" defaultValue="Everyone" className="form-control mb-3" />
+                <div className="dropdown">
+                  <button className="btn btn-secondary dropdown-toggle" type="button" id="assignDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                    {assignEveryone ? "Everyone" : "Selected Students"}
+                  </button>
+                  <ul className="dropdown-menu" aria-labelledby="assignDropdown">
+                    <li>
+                      <label className="dropdown-item">
+                        <input
+                          type="checkbox"
+                          id="assign-everyone"
+                          className="me-2"
+                          checked={assignEveryone}
+                          onChange={handleEveryoneChange}
+                        />
+                        Everyone
+                      </label>
+                    </li>
+                    {students.map(student => (
+                      <li key={student._id}>
+                        <label className={`dropdown-item ${assignEveryone ? "wd-disabled-label" : ""}`}>
+                          <input
+                            type="checkbox"
+                            id={`assign-${student._id}`}
+                            className={`me-2 ${assignEveryone ? "wd-disabled-checkbox" : ""}`}
+                            checked={assignTo.includes(student._id)}
+                            disabled={assignEveryone}
+                            onChange={() => renderAssignChange(student._id)}
+                          />
+                          {student.firstName} {student.lastName}
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                {!assignEveryone && (
+                  <div className="mt-2">
+                    {selectedStudents.map((student, index) => (
+                      <span key={index} className="badge bg-light text-dark me-1">{student}</span>
+                    ))}
+                  </div>
+                )}
               </div>
             </div>
 
@@ -68,7 +147,7 @@ export default function AssignmentEditor() {
                 <label htmlFor="wd-due-date" className="form-label"><strong>Due</strong></label>
               </div>
               <div className="col-md-10">
-                <input id="wd-due-date" type="datetime-local" className="form-control" defaultValue={assignment.dueDate || ''} />
+                <input id="wd-due-date" type="datetime-local" className="form-control" defaultValue={assignment.dueDate || ""} />
               </div>
             </div>
 
@@ -77,13 +156,13 @@ export default function AssignmentEditor() {
                 <label htmlFor="wd-available-from" className="form-label"><strong>Available From</strong></label>
               </div>
               <div className="col-md-5">
-                <input id="wd-available-from" type="datetime-local" className="form-control" defaultValue={assignment.availableFrom || ''} />
+                <input id="wd-available-from" type="datetime-local" className="form-control" defaultValue={assignment.availableFrom || ""} />
               </div>
               <div className="col-md-1">
                 <label htmlFor="wd-available-until" className="form-label"><strong>Until</strong></label>
               </div>
               <div className="col-md-4">
-                <input id="wd-available-until" type="datetime-local" className="form-control" defaultValue={assignment.availableUntil || ''} />
+                <input id="wd-available-until" type="datetime-local" className="form-control" defaultValue={assignment.availableUntil || ""} />
               </div>
             </div>
           </div>
